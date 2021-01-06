00f87d0ab5e44409fa6ac4a37454457160e85571
Harshit1109 <77053371+Harshit1109@users.noreply.github.com>
2021-01-06 17:30:26 +0530

    Create FizzAt3multiples.java

---
+++ FizzAt3multiples.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class FizzAt3multiples{
+	public static void main(String[] args){
+		for(int i=1;i<=15;i++){
+			if(i%3==0){
+			System.out.println("Fizz");	
+			} else
+			System.out.println(i);
+		}	
+	}
+}
