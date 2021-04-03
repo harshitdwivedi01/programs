66bfbff128a7835fdc41857dc82c77965447ea97
Harshit Dwivedi <77053371+Harshit1109@users.noreply.github.com>
2021-04-03 11:02:24 +0530

    ArrayList.java program

---
+++ ArrayL.java
@@ -0,0 +1,11 @@
+//Using Arraylist
+
+import java.util.*;
+class ArrayL{
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("User");
+		list.add("Owner");
+		System.out.println(list); 
+  }
+}
