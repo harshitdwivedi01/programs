192a3c958c20f796e4000bb1f95c20b3ae18d2d9
Harshit Dwivedi <77053371+Harshit1109@users.noreply.github.com>
2021-05-10 09:13:50 +0530

    Update HighLow.java

--- HighLow.java
+++ HighLow.java
@@ -1,5 +1,6 @@
 //CODEWARS              highAndLow("1 2 3 4 5")  // return "5 1"
-//In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.import java.util.Scanner;
+//In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+import java.util.Scanner;
 
 
 class HighLow{
